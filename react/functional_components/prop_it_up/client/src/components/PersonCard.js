c24c1e4591eae969189fc97004ea44585163fca0
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-04 19:31:49 -0700

    first commit

---
+++ react/functional_components/prop_it_up/client/src/components/PersonCard.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const PersonCard = (props) => {
+    return(
+        <>
+            <h1>{props.lastName}, {props.firstName}</h1>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.color}</p>
+        </>
+    )
+}
+
+export default PersonCard
