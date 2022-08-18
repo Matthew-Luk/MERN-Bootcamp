e7514dfc88c21f2e872ea94b8d9e6c21343438d6
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-17 18:52:34 -0700

    first commit

--- power_ups/power_up3/client/src/components/Form.js
+++ power_ups/power_up3/client/src/components/Form.js
@@ -2,29 +2,29 @@ import React from 'react'
 
 const Form = (props) => {
 
-    const {name,email,password,setName,setEmail,setPassword,setNewUser,newUser} = props
+    const {userName,userEmail,userPassword,setUserName,setUserEmail,setUserPassword,setNewUser,newUser} = props
 
     const handleSubmit = (e) => {
-        e.preventDeafault()
+        e.preventDefault()
         const newAddition = {
-            name,
-            email,
-            password
+            userName,
+            userEmail,
+            userPassword
         }
         console.log(`Welcome ${newAddition}`)
         setNewUser([...newUser,newAddition])
     }
 
     const handleName = (e) => {
-        setName(e.target.value)
+        setUserName(e.target.value)
     }
 
     const handleEmail = (e) => {
-        setEmail(e.target.value)
+        setUserEmail(e.target.value)
     }
 
     const handlePassword = (e) => {
-        setPassword(e.target.value)
+        setUserPassword(e.target.value)
     }
 
     const styledDiv = {
@@ -63,25 +63,29 @@ const Form = (props) => {
     return (
         <div style={styledDiv}>
             <form onSubmit={handleSubmit}>
-                <h2>Form</h2>
-                <div style={styledMargin}>
-                <label htmlFor="name">Name:</label>
-                <input style={styledBorder} type="text" name="name" onChange={handleName}/>
-                </div>
-                <div style={styledMargin}>
-                <label htmlFor="email">Email:</label>
-                <input style={styledBorder} type="text" name="email" onChange={handleEmail}/>
-                </div>
-                <div style={styledMargin}>
-                <label htmlFor="password">Password:</label>
-                <input style={styledBorder} type="password" name="password" onChange={handlePassword}/>
-                </div>
-                <div style={styledMargin}>
+            <h2>Form</h2>
+            <div style={styledMargin}>
+            <label htmlFor="">Name:</label>
+            <input value={userName} style={styledBorder} type="text" name="name" onChange={handleName}/>
+            </div>
+            <div style={styledMargin}>
+            <label htmlFor="">Email:</label>
+            <input value={userEmail} style={styledBorder} type="text" name="email" onChange={handleEmail}/>
+            </div>
+            <div style={styledMargin}>
+            <label htmlFor="">Password:</label>
+            <input value={userPassword} style={styledBorder} type="text" name="password" onChange={handlePassword}/>
+            </div>
+            <div style={styledMargin}>
                 <button style={styledButton} onClick={handleSubmit}>Submit</button>
-                </div>
+            </div>
+            
+    
+    
+    
             </form>
         </div>
-    )
-}
+        )
+    }
 
 export default Form
