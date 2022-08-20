0dd50c78a13b811c7bc10c6805ad7ad240faa136
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-19 20:38:37 -0700

    first commit

---
+++ react/functional_components/react_blocks/client/src/components/SubContents.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../static/App.module.css'
+
+const SubContents = () => {
+    return (
+        <div className={styles.subcontents}>
+        </div>
+    )
+}
+
+export default SubContents
