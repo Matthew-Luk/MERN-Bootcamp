f4765932334fc02dba50be2ef8a047da9db3865f
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-19 20:39:23 -0700

    first commit

---
+++ react/functional_components/react_blocks/client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
+import SubContents from './components/SubContents';
+import Advertisement from './components/Advertisement';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className='top'>
+        <Header />
+      </div>
+      <div className='bottom'>
+        <Navigation />
+        <Main >
+          <div className='main-top'>
+            <SubContents />
+            <SubContents />
+            <SubContents />
+          </div>
+          <div className='main-bottom'>
+            <Advertisement />
+          </div>
+        </Main>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
