f516fa01989fd886a929f7e20c85d5ea3aae4954
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-19 20:38:43 -0700

    first commit

---
+++ react/functional_components/react_blocks/client/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../static/App.module.css'
+
+const Navigation = () => {
+    return (
+        <div className={styles.navigation}>
+        </div>
+    )
+}
+
+export default Navigation
