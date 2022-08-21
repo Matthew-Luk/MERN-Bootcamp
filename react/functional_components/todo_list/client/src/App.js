9458511d10770dc5ecf18263c567de592f47f922
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-20 17:18:03 -0700

    first commit

---
+++ react/functional_components/todo_list/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Form from './components/Form';
+import Todo from './components/Todo';
+import List from './components/List';
+import {useState} from 'react'
+
+function App() {
+
+  const [todoList,setTodoList] = useState([])
+
+  return (
+    <div className="App">
+      <Form todoList={todoList} setTodoList={setTodoList}/>
+      <List todoList={todoList} setTodoList={setTodoList}/>
+    </div>
+  );
+}
+
+export default App;
