40b5919b376af3edef008e5a56626acad046ebad
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-20 17:17:56 -0700

    first commit

---
+++ react/functional_components/todo_list/client/src/components/Form.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+
+const styledDiv = {
+    border:'2px solid black',
+    padding: '20px'
+}
+
+const Form = (props) => {
+
+    const {todoList,setTodoList} = props
+    const [todo,setTodo] = useState({})
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if(todo.todo.trim() === ""){
+            return
+        }
+        setTodoList([...todoList,todo])
+        setTodo({todo:"",completed:""})
+    }
+
+    const inputHandler = (e) => {
+        setTodo({todo:e.target.value,completed:false})
+    }
+
+    return (
+        <div style={styledDiv}>
+            <form onSubmit={submitHandler}>
+                <div>
+                    <label htmlFor="">Todo: </label>
+                    <input value={todo.todo} onChange={inputHandler} type="text"></input>
+                </div>
+                <div><button>Submit</button></div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
