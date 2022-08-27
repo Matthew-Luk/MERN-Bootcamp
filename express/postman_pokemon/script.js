76dd352afaa708bfb7bb8914b816179f7cce247d
Matthew Luk <Matthew.Luk@yahoo.com>
2022-08-26 18:34:56 -0700

    first commit

---
+++ express/postman_pokemon/script.js
@@ -0,0 +1,57 @@
+// For this entire assignment I am only taking the portion of JSON that has the name.
+// Postman returns 12,000 lines of code per GET and I don't wan't to copy all of that.
+
+
+// 1. https://pokeapi.co/api/v2/pokemon/1
+[{
+"name": "bulbasaur",
+"order": 1,
+"past_types": [],
+"species": {
+    "name": "bulbasaur",
+    "url": "https://pokeapi.co/api/v2/pokemon-species/1/"
+}}]
+
+
+// 2. https://pokeapi.co/api/v2/pokemon/200
+[{
+"name": "misdreavus",
+"order": 295,
+"past_types": [],
+"species": {
+    "name": "misdreavus",
+    "url": "https://pokeapi.co/api/v2/pokemon-species/200/"
+}}]
+
+
+// 3. https://pokeapi.co/api/v2/pokemon/pikachu
+[{
+"name": "pikachu",
+"order": 35,
+"past_types": [],
+"species": {
+    "name": "pikachu",
+    "url": "https://pokeapi.co/api/v2/pokemon-species/25/"
+}}]
+
+
+// 4. https://pokeapi.co/api/v2/pokemon/blastoise
+[{
+"name": "blastoise",
+"order": 12,
+"past_types": [],
+"species": {
+    "name": "blastoise",
+    "url": "https://pokeapi.co/api/v2/pokemon-species/9/"
+}}]
+
+
+// 5. https://pokeapi.co/api/v2/pokemon/charizard
+[{
+"name": "charizard",
+"order": 7,
+"past_types": [],
+"species": {
+    "name": "charizard",
+    "url": "https://pokeapi.co/api/v2/pokemon-species/6/"
+}}]
