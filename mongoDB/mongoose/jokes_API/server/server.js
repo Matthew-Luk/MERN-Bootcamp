5ca67a966b07e196a0329c964a44cb31419bfc63
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-03 15:19:09 -0700

    first commit

---
+++ mongoDB/mongoose/jokes_API/server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const mongoose = require('mongoose')
+
+app.use(express.json())
+
+require("./config/mongoose.config.js")
+
+require('./routes/jokes.routes')(app)
+
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
