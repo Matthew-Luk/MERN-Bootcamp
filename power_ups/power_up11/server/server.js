1cc17cb776c7ec261340f79d29194924f98ba5b6
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-06 19:57:50 -0700

    first commit

--- power_ups/power_up11/server/server.js
+++ power_ups/power_up11/server/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const port = 8000
+const cors = require('cors')
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
 
 //Config
 require("./config/mongoose.config")
