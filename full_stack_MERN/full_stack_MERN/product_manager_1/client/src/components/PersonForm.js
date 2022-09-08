b267db963329a1f3b5736f26fbfc2397e69d8144
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-07 18:55:25 -0700

    first commit

---
+++ full_stack_MERN/full_stack_MERN/product_manager_1/client/src/components/PersonForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import '../App.css';
+
+const PersonForm = () => {
+
+    const [title, setTitle] = useState(""); 
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+
+    const onSubmitHandler = (e) => {
+
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/product', {
+            title,
+            price,
+            description
+        })
+            .then(res=>{
+                console.log(res);
+                console.log(res.data);
+                setTitle("")
+                setPrice("")
+                setDescription("")
+            })
+            .catch(err=>console.log(err))
+    }
+
+    const titleHandler = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const priceHandler = (e) => {
+        setPrice(e.target.value)
+    }
+
+    const descriptionHandler = (e) => {
+        setDescription(e.target.value)
+    }
+    
+    return (
+        <form className="form" onSubmit={onSubmitHandler}>
+            <h1>Product Manager</h1>
+            <div className='inputblocks'>
+                <label>Title</label><br/>
+                <input className='input' type="text" onChange = {titleHandler} value={title}/>
+            </div>
+            <div className='inputblocks'>
+                <label>Price $</label><br/>
+                <input className='input' type="text" onChange = {priceHandler} value={price}/>
+            </div>
+            <div className='inputblocks'>
+                <label>Description</label><br/>
+                <input className='input' type="text" onChange = {descriptionHandler} value={description}/>
+            </div>
+            <div>
+                <input className='createbtn' type="submit" value="Create"/>
+            </div>
+        </form>
+    )
+}
+export default PersonForm;
