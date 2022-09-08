0432f2fbced7bcb00234a023d6a5542384d0ea27
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-07 18:55:34 -0700

    first commit

---
+++ full_stack_MERN/full_stack_MERN/product_manager_1/server/controllers/product.controller.js
@@ -0,0 +1,46 @@
+const Product = require('../models/product.model');
+
+const ProductController = {
+
+    create:(req,res) => {
+        Product.create(req.body)
+        .then((product)=>{
+            res.status(201).json({product:product})
+        })
+        .catch((err)=>{
+            res.status(400).json({message:"Something went wrong",error:err})
+        })
+    },
+
+    getAll:(req,res) => {
+        Product.find({})
+        .then((product) => {
+            res.status(200).json({product:product})
+        })
+        .catch((err)=>{
+            res.status(400).json({message:"Something went wrong",error:err})
+        })
+    },
+
+    deleteOne:(req,res) => {
+        Product.findByIdAndDelete({_id:req.params.id})
+        .then((products)=>{
+            res.status(200).json({deletedProduct:products})
+        })
+        .catch((err)=>{
+            res.status(500).json({message:"Something went wrong",error:err})
+        })
+    },
+
+    deleteAll:(req,res) => {
+        Product.deleteMany({})
+        .then((products)=>{
+            res.status(200).json({deletedProducts:products})
+        })
+        .catch((err)=>{
+            res.status(500).json({message:"Something went wrong",error:err})
+        })
+    }
+}
+
+module.exports = ProductController
