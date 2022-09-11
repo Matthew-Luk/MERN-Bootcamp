2b4f4e787e420333d8cb88059d598582297f9977
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-10 22:57:41 -0700

    first commit

---
+++ full_stack_MERN/full_stack_MERN/product_manager_3/client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import {BrowserRouter, Routes, Router, Route, Navigate} from 'react-router-dom';
+import Main from './views/Main';
+import OneProduct from './components/OneProduct';
+import Update from './components/Update';
+
+
+function App() {
+  return(
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Navigate to ='/home'/>}/>
+            <Route element={<Main/>} path="/home" default />
+            <Route element={<OneProduct/>} path="/product/:id" />
+            <Route element={<Update />} path="/product/edit/:id" />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    ) 
+  }
+export default App;
