d7f7d5d71f7e9ddead0efdba29b345744ca0063b
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-11 11:23:21 -0700

    first commit

---
+++ full_stack_MERN/full_stack_MERN/product_manager_3/client/src/views/Main.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import ProductForm from '../components/ProductForm';
+import ProductList from '../components/ProductList';
+import '../App.css';
+
+
+const Main = (props) => {
+    
+    const [productList, setProductList] = useState([])
+
+    const removeFromDom = (productId) => {
+        setProductList(productList.filter(product => product._id != productId))
+    }
+    
+    return (
+        <div className='main'>
+            <ProductForm productList={productList} setProductList={setProductList} />
+            <hr/>
+            <ProductList productList={productList} setProductList={setProductList} removeFromDom={removeFromDom}/>
+        </div>
+    )
+}
+export default Main;
