2cd75da0d66a93201384bcdc536b376a226f9df3
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-12 18:55:07 -0700

    first commit

---
+++ full_stack_MERN/advanced_MERN/authors/client/src/components/AllAuthors.js
@@ -0,0 +1,63 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { Link , useNavigate} from 'react-router-dom'
+import '../App.css';
+
+const AllAuthors = (props) => {
+
+    const {removeFromDom,authorList,setAuthorList} = props
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/authors")
+        .then((res) => {
+            console.log(res.data.authors)
+            setAuthorList(res.data.authors)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    },[])
+
+    const handleDelete = (authorId) => {
+        axios.delete("http://localhost:8000/api/authors/" + authorId)
+        .then((res) => {
+            removeFromDom(authorId)
+            navigate('/')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    return (
+        <div>
+            <h2>Favorite Authors</h2>
+            <Link to={'/new'}>Add an Author</Link>
+            <h3>We have quotes by: </h3>
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Authors</th>
+                        <th>Actions Available</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        authorList.map((item,index)=>(
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>
+                                    <Link to={`/edit/${item._id}`}><button className='btn'>Edit</button></Link>
+                                    <button className='btn' onClick={(e) => {handleDelete(item._id)}}>Delete</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default AllAuthors
