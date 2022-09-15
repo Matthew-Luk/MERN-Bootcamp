b1c3b886a67ffb352f7ce5e79ed09c0296256f25
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-14 21:32:33 -0700

    first commit

---
+++ react/react_routing/luke_APIwalker/client/src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+const Error = () => {
+    return (
+        <div>
+            <img src="https://www.meme-arsenal.com/memes/52577612a290566287f2273992fa918e.jpg"/>
+        </div>
+    )
+}
+
+export default Error
