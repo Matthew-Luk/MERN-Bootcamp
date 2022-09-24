5f4956335b65c051b9143262356000461e4bf338
Matthew Luk <Matthew.Luk@yahoo.com>
2022-09-23 20:18:46 -0700

    update

--- full_stack_MERN/advanced_MERN/team_manager/client/src/components/ManagePlayers1.js
+++ full_stack_MERN/advanced_MERN/team_manager/client/src/components/ManagePlayers1.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import '../App.css';
 
-const ManagePlayers1 = () => {
+const ManagePlayers1 = (props) => {
+
+    const {playerList} = props
+    const [btn1,setBtn1] = useState("")
+    const [btn2,setBtn2] = useState("")
+    const [btn3,setBtn3] = useState("")
+
     return (
         <div>
             <div className='navbar1'>
@@ -10,10 +17,36 @@ const ManagePlayers1 = () => {
             </div>
             <div className='content2'>
                 <div className='navbar3'>
-                    <h2>Player Status - Game 1</h2>
-                    <Link>Game 1| </Link>
-                    <Link>Game 2| </Link>
-                    <Link>Game 3</Link>
+                    <h1>Player Status - Game 1</h1>
+                    <div className='gamelinks'>
+                        <Link to={'/status/game/1'}>Game 1| </Link>
+                        <Link to={'/status/game/2'}>Game 2| </Link>
+                        <Link to={'/status/game/3'}>Game 3</Link>
+                    </div>
+                </div>
+                <div>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Player Name</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                playerList.map((item,index) => (
+                                    <tr key={index}>
+                                        <td className='goop'><Link to={`/players/edit/${item._id}`}>{item.name}</Link></td>
+                                        <td>
+                                            <button id='playingbtn'>Playing</button>
+                                            <button id='notplayingbtn'>Not Playing</button>
+                                            <button id='undecidedbtn'>Undecided</button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
