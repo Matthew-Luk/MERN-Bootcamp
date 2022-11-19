c263e6a69fa277a4c8bed6aa0e2a0195e1b32f43
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-19 15:04:31 -0800

    update syntax and added css

--- react/functional_components/putting_it_together/client/src/components/PersonCard.js
+++ react/functional_components/putting_it_together/client/src/components/PersonCard.js
@@ -1,15 +1,18 @@
 import React, {useState} from "react"
+import '../App.css';
 
 const PersonCard = (props) => {
-    const {firstName,lastName,age,color} = props
+
+    const {firstName,lastName,age,hairColor} = props
     const [year,setYear] = useState(age);
+
     return(
         <>
             <h1>{lastName}, {firstName}</h1>
             <p>Age: {year}</p>
-            <p>Hair Color: {color}</p>
-            <button onClick ={(event) => setYear(year+1)}>Add a year to {firstName}</button>
-            <button onClick ={(event) => setYear(year-1)}>Subtract a year from {firstName}</button>
+            <p>Hair Color: {hairColor}</p>
+            <button className="btn" onClick ={() => setYear(year+1)}>Add a year to {firstName}</button>
+            <button className="btn" onClick ={() => setYear(year-1)}>Subtract a year from {firstName}</button>
         </>
     )
 }
