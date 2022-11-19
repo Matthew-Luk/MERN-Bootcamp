1a1dd07ab9f600c91b0fcbb5d7430af9fd427a2d
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-19 15:32:52 -0800

    update naming convention and cleaned form

--- react/functional_components/hook_form/client/src/components/UserForm.js
+++ react/functional_components/hook_form/client/src/components/UserForm.js
@@ -1,46 +1,58 @@
 import React, {useState} from 'react'
 
 const UserForm = (props) => {
-    const [ userfirstname, setFirstname] = useState("")
-    const [ userlastname,setLastname] = useState("")
-    const [ useremail,setEmail] = useState("")
-    const [ password,setPassword] = useState("")
-    const [ confirmpassword,setConfirmpassword] = useState("")
+    const [ firstName, setFirstName ] = useState("")
+    const [ lastName, setLastName ] = useState("")
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
+    const [ confirmPassword, setConfirmPassword ] = useState("")
+
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = {userfirstname, userlastname, useremail, password, confirmpassword}
+        const newUser = {firstName, lastName, email, password, confirmPassword}
         console.log("Welcome", newUser)
+        setFirstName("")
+        setLastName("")
+        setEmail("")
+        setPassword("")
+        setConfirmPassword("")
     }
+
+    // I wanted to try two different ways of handling inputs
+    const firstNameHandler = (e) => {
+        setFirstName(e.target.value)
+    }
+
     return (
-        <form onSubmit={createUser}>
-            <div>
+        <form className='form' onSubmit={createUser}>
+            <div className='styledDiv'>
                 <label htmlFor="firstname">First name: </label>
-                <input type="text" name="firstname" onChange={(e) => setFirstname(e.target.value)}></input>
+                <input type="text" name="firstname" onChange={firstNameHandler} value={firstName}></input>
             </div>
-            <div>
+            <div className='styledDiv'>
                 <label htmlFor='lastname'>Last name: </label>
-                <input type="text" name="lastname" onChange={(e) => setLastname(e.target.value)}></input>
+                <input type="text" name="lastname" onChange={(e) => setLastName(e.target.value)} value={lastName}></input>
             </div>
-            <div>
+            <div className='styledDiv'>
                 <label htmlFor='email'>Email: </label>
-                <input type="text" name='email' onChange={(e) => setEmail(e.target.value)}></input>
+                <input type="text" name='email' onChange={(e) => setEmail(e.target.value)} value={email}></input>
             </div>
-            <div>
+            <div className='styledDiv'>
                 <label htmlFor='password'>Password: </label>
-                <input type="password" name='password' onChange={(e) => setPassword(e.target.value)}></input>
+                <input type="password" name='password' onChange={(e) => setPassword(e.target.value)} value={password}></input>
             </div>
-            <div>
-                <label htmlFor='confirmpassword'>Confirm Password: </label>
-                <input type="password" name='confirmpassword' onChange={(e) => setConfirmpassword(e.target.value)}></input>
+            <div className='styledDiv'>
+                <label htmlFor='confirmPassword'>Confirm Password: </label>
+                <input type="password" name='confirmPassword' onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}></input>
             </div>
-            <input type="submit" value="Create User"></input>
-            <div>
+            <input id='submit_btn' type="submit" value="Create User"></input>
+            <div className='dataDiv'>
                 <h2>Your Form Data</h2>
-                <p><label>First Name: </label>{ userfirstname }</p>
-                <p><label>Last Name: </label>{ userlastname }</p>
-                <p><label>Email: </label>{ useremail }</p>
-                <p><label>Password: </label>{ password }</p>
-                <p><label>Confirm Password: </label>{ confirmpassword }</p>
+                <p><label>First Name:</label> { firstName }</p>
+                <p><label>Last Name:</label> { lastName }</p>
+                <p><label>Email:</label> { email }</p>
+                <p><label>Password:</label> { password }</p>
+                <p><label>Confirm Password:</label> { confirmPassword }</p>
             </div>
         </form>
     )
