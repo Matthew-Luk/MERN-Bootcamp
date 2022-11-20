0e7651a2107b33caf35dd91b95356faccb004428
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-20 14:32:51 -0800

    update style tags and styles

--- react/functional_components/navbar_context/client/src/components/Form.js
+++ react/functional_components/navbar_context/client/src/components/Form.js
@@ -4,9 +4,18 @@ import UserContext from '../contexts/UserContext'
 const styledDiv = {
     display: 'flex',
     justifyContent: 'center',
+    alignItems: 'center',
     marginTop: '20px'
 }
 
+const styledLabel = {
+    marginRight: ' 15px'
+}
+
+const styledInput = {
+    height: '30px',
+    borderRadius: '5px'
+}
 
 const Form = () => {
 
@@ -14,8 +23,8 @@ const Form = () => {
 
     return (
         <div style={styledDiv}>
-            <label htmlFor="">Your Name: </label>
-            <input type="text" onChange={e => setState(e.target.value)}/>
+            <label style={styledLabel} htmlFor="">Your Name: </label>
+            <input style={styledInput} type="text" onChange={e => setState(e.target.value)}/>
         </div>
     )
 }
