5b3311c6c4664708a65ec61bbe09646b0ffc9b76
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-20 14:32:08 -0800

    update

--- react/functional_components/more_forms/client/src/components/UserForm.js
+++ react/functional_components/more_forms/client/src/components/UserForm.js
@@ -1,103 +1,93 @@
 import React, {useState} from 'react'
 
 const UserForm = (props) => {
-    const [ firstName, setFirstName] = useState("")
-    const [ firstNameError, setFirstNameError] = useState("")
-    const [ lastName,setLastName] = useState("")
-    const [ lastNameError, setLastNameError] = useState("")
-    const [ email,setEmail] = useState("")
-    const [ emailError, setEmailError] = useState("")
-    const [ password,setPassword] = useState("")
-    const [ passwordError,setPasswordError] = useState("")
-    const [ confirmPassword,setConfirmPassword] = useState("")
-    const handleFirstName = (e) => {
-        setFirstName(e.target.value);
-        if(e.target.value.length < 2){
-            setFirstNameError("First name must be at least 2 characters")
-        }else{
-            setFirstNameError("")
-        }
-    }
-    const handleLastName = (e) => {
-        setLastName(e.target.value);
-        if(e.target.value.length < 2){
-            setLastNameError("Last name must be at least 2 characters")
-        }else{
-            setLastNameError("")
-        }
-    }
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-        if(e.target.value.length < 5){
-            setEmailError("Email be at least 5 characters")
-        }else{
-            setEmailError("")
-        }
-    }
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-        if(e.target.value.length < 8){
-            setPasswordError("Password must be at least 8 characters")
-        }else{
-            setPasswordError("")
-        }
-    }
-    const passwordMatch = (e) => {
-        setPassword(e.target.value);
 
+    const [ firstName, setFirstName] = useState({name:"", error:""})
+    const [ lastName, setLastName] = useState({name:"", error:""})
+    const [ email, setEmail] = useState({email:"", error:""})
+    const [ password, setPassword] = useState({pw:"", error:""})
+    const [ confirmPassword, setConfirmPassword] = useState("")
+    const [ hasBeenSubmitted, setHasBeenSubmitted] = useState(false)
+
+    const createUser = (e) => {
+        e.preventDefault()
+        setFirstName({name:"", error:""})
+        setLastName({name:"", error:""})
+        setEmail({email:"", error:""})
+        setPassword({pw:"", error:""})
+        setConfirmPassword("")
+        setHasBeenSubmitted(true)
     }
+
+    const formMessage = () => {
+        if( hasBeenSubmitted ) {
+            return "Thank you for submitting the form!";
+        } else {
+            return "Welcome, please submit the form";
+        }
+    };
+
+    const handleFirstName = (e) => {
+        if(e.target.value.length < 2){
+            setFirstName({name:"", error:"First name must be at least 2 characters"})
+        }else{
+            setFirstName({name:(e.target.value), error:""})
+        }
+    }
+
+    const handleLastName = (e) => {
+        if(e.target.value.length < 2){
+            setLastName({name:"", error:"Last name must be at least 2 characters"})
+        }else{
+            setLastName({name:(e.target.value), error:""})
+        }
+    }
+
+    const handleEmail = (e) => {
+        if(e.target.value.length < 5){
+            setEmail({email:"", error:"Email be at least 5 characters"})
+        }else{
+            setEmail({email:(e.target.value), error:""})
+        }
+    }
+
+    const handlePassword = (e) => {
+        if(e.target.value.length < 8){
+            setPassword({pw:"", error:"Password must be at least 8 characters"})
+        }else{
+            setPassword({pw:(e.target.value), error:""})
+        }
+    }
+
     return (
-        <form onSubmit={ (e) => e.preventDefault() }>
-            <div>
+        <form className='form' onSubmit={ createUser }>
+            <h3>{formMessage()}</h3>
+            <div className='styledDiv'>
                 <label htmlFor="firstname">First name: </label>
-                <input type="text" name="firstname" onChange={ handleFirstName }></input>
-                {
-                    firstNameError ?
-                    <p>{firstNameError}</p>:
-                    ''
-                }
+                <input type="text" name="firstname" onChange={ handleFirstName } value={firstName.name}></input>
             </div>
-            <div>
+                {firstName.error ? <p>{firstName.error}</p> : ''}
+            <div className='styledDiv'>
                 <label htmlFor='lastname'>Last name: </label>
-                <input type="text" name="lastname" onChange={ handleLastName }></input>
-                {
-                    lastNameError ?
-                    <p>{lastNameError}</p>:
-                    ''
-                }
+                <input type="text" name="lastname" onChange={ handleLastName } value={lastName.name}></input>
             </div>
-            <div>
+                {lastName.error ? <p>{lastName.error}</p> : ''}
+            <div className='styledDiv'>
                 <label htmlFor='email'>Email: </label>
-                <input type="text" name='email' onChange={ handleEmail }></input>
-                {
-                    emailError ?
-                    <p>{emailError}</p>:
-                    ''
-                }
+                <input type="text" name='email' onChange={ handleEmail } value={email.email}></input>
             </div>
-            <div>
+                {email.error ? <p>{email.error}</p> : ''}
+            <div className='styledDiv'>
                 <label htmlFor='password'>Password: </label>
                 <input type="password" name='password' onChange={ handlePassword }></input>
-                {
-                    passwordError ?
-                    <p>{passwordError}</p>:
-                    ''
-                }
             </div>
-            <div>
+                {password.error ? <p>{password.error}</p> : ''}
+            <div className='styledDiv'>
                 <label htmlFor='confirmPassword'>Confirm Password: </label>
-                <input type="password" name='confirmPassword' onChange={(e) => setConfirmPassword(e.target.value)}></input>
+                <input type="password" name='confirmPassword' onChange={(e) => setConfirmPassword(e.target.value)} value={confirmPassword}></input>
             </div>
-                {confirmPassword !== password ? <p>Passwords must match</p> : null}
-            <input type="submit" value="Create User"></input>
-            <div>
-                <h2>Your Form Data</h2>
-                <p><label>First Name: </label>{ firstName }</p>
-                <p><label>Last Name: </label>{ lastName }</p>
-                <p><label>Email: </label>{ email }</p>
-                <p><label>Password: </label>{ password }</p>
-                <p><label>Confirm Password: </label>{ confirmPassword }</p>
-            </div>
+            {confirmPassword !== password.pw ? <p>Passwords must match</p> : null}
+            <input id='submit_btn' type="submit" value="Create User"></input>
         </form>
     )
 }
