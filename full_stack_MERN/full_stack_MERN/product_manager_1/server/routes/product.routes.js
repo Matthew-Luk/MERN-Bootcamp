427a6b40d166e7c8e3f0fb7cae590fc497ee2320
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-25 12:44:30 -0800

    added CRUD notes

--- full_stack_MERN/full_stack_MERN/product_manager_1/server/routes/product.routes.js
+++ full_stack_MERN/full_stack_MERN/product_manager_1/server/routes/product.routes.js
@@ -1,8 +1,15 @@
 const ProductController = require('../controllers/product.controller');
 
 module.exports = (app) => {
+    // Get All
     app.get('/api/product', ProductController.getAll);
+
+    // Create
     app.post('/api/product', ProductController.create);
+
+    // Delete One
     app.delete('/api/product/:id', ProductController.deleteOne)
+
+    // Delete All
     app.delete('/api/product', ProductController.deleteAll)
 }
