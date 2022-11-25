07ed3a6c53594cde7abc8d2034a59244fe4c4dd0
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-25 12:43:58 -0800

    fixed spacing

--- full_stack_MERN/full_stack_MERN/product_manager_1/server/server.js
+++ full_stack_MERN/full_stack_MERN/product_manager_1/server/server.js
@@ -9,5 +9,4 @@ app.use(express.urlencoded({ extended: true }));
 require('./config/mongoose.config');
 require('./routes/product.routes')(app);
 
-
 app.listen(8000, () => {console.log("Listening at Port 8000")})
