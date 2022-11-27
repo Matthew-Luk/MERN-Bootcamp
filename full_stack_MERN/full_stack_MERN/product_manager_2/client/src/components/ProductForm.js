81c228b25c2719f874f4d37244edf56b11427609
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-26 23:09:35 -0800

    update

--- full_stack_MERN/full_stack_MERN/product_manager_2/client/src/components/ProductForm.js
+++ full_stack_MERN/full_stack_MERN/product_manager_2/client/src/components/ProductForm.js
@@ -12,7 +12,6 @@ const PersonForm = (props) => {
     const navigate = useNavigate()
 
     const onSubmitHandler = (e) => {
-
         e.preventDefault();
         axios.post('http://localhost:8000/api/product', {
             title,
@@ -42,29 +41,29 @@ const PersonForm = (props) => {
     const descriptionHandler = (e) => {
         setDescription(e.target.value)
     }
-    
+
     return (
         <>
             <form className="form" onSubmit={onSubmitHandler}>
-            <h1>Product Manager</h1>
-            <div className='inputblocks'>
-                <label>Title</label><br/>
-                <input className='input' type="text" onChange = {titleHandler} value={title}/>
-            </div>
-            <div className='inputblocks'>
-                <label>Price $</label><br/>
-                <input className='input' type="text" onChange = {priceHandler} value={price}/>
-            </div>
-            <div className='inputblocks'>
-                <label>Description</label><br/>
-                <input className='input' type="text" onChange = {descriptionHandler} value={description}/>
-            </div>
-            <div>
-                <input className='createbtn' type="submit" value="Create"/>
-            </div>
-        </form>
+                <h1>Product Manager</h1>
+                <div className='inputblocks'>
+                    <label>Title</label><br/>
+                    <input className='input' type="text" onChange = {titleHandler} value={title}/>
+                </div>
+                <div className='inputblocks'>
+                    <label>Price $</label><br/>
+                    <input className='input' type="text" onChange = {priceHandler} value={price}/>
+                </div>
+                <div className='inputblocks'>
+                    <label>Description</label><br/>
+                    <input className='input' type="text" onChange = {descriptionHandler} value={description}/>
+                </div>
+                <div>
+                    <input className='createbtn' type="submit" value="Create"/>
+                </div>
+            </form>
         </>
-        
     )
 }
+
 export default PersonForm;
