055f1b55c6cb4fc62ed7ac37040b3efe00ff34b6
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-27 20:08:48 -0800

    added notes

--- full_stack_MERN/full_stack_MERN/product_manager_3/server/routes/product.routes.js
+++ full_stack_MERN/full_stack_MERN/product_manager_3/server/routes/product.routes.js
@@ -1,10 +1,21 @@
 const ProductController = require('../controllers/product.controller');
 
 module.exports = (app) => {
+    // Get All
     app.get('/api/product', ProductController.getAll);
+
+    // Get One
     app.get('/api/product/:id', ProductController.getOne);
+
+    // Create
     app.post('/api/product', ProductController.create);
-    app.put('/api/product/:id', ProductController.update)
-    app.delete('/api/product', ProductController.deleteAll)
-    app.delete('/api/product/:id', ProductController.deleteOne)
+
+    // Update
+    app.put('/api/product/:id', ProductController.update);
+
+    // Delete All
+    app.delete('/api/product', ProductController.deleteAll);
+
+    // Delete One
+    app.delete('/api/product/:id', ProductController.deleteOne);
 }
