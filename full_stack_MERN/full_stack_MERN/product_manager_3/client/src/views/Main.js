dccf1f612042f6bd0fc716afe334191229799823
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-27 20:08:26 -0800

    update

--- full_stack_MERN/full_stack_MERN/product_manager_3/client/src/views/Main.js
+++ full_stack_MERN/full_stack_MERN/product_manager_3/client/src/views/Main.js
@@ -3,15 +3,14 @@ import ProductForm from '../components/ProductForm';
 import ProductList from '../components/ProductList';
 import '../App.css';
 
-
 const Main = (props) => {
-    
+
     const [productList, setProductList] = useState([])
 
     const removeFromDom = (productId) => {
         setProductList(productList.filter(product => product._id != productId))
     }
-    
+
     return (
         <div className='main'>
             <ProductForm productList={productList} setProductList={setProductList} />
@@ -20,4 +19,5 @@ const Main = (props) => {
         </div>
     )
 }
+
 export default Main;
