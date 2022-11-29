4282da27dbd26717e79cef297134cf840bdc25c3
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-28 21:11:56 -0800

    update

--- full_stack_MERN/advanced_MERN/authors/server/models/author.model.js
+++ full_stack_MERN/advanced_MERN/authors/server/models/author.model.js
@@ -4,7 +4,7 @@ const AuthorSchema = new mongoose.Schema({
     name:{
         type:String,
         required:[true,"Author's name is required"],
-        minLenth:[3,"Name must be at least 3 characters"]
+        minlength:[3,"Name must be at least 3 characters"]
     }
 }, {timestamps:true})
 
