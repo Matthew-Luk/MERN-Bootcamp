f36ebaa82440334a0b8c62e1599fd686b441041f
Matthew Luk <Matthew.Luk@yahoo.com>
2022-11-28 21:12:23 -0800

    update

--- full_stack_MERN/advanced_MERN/team_manager/server/models/player.model.js
+++ full_stack_MERN/advanced_MERN/team_manager/server/models/player.model.js
@@ -4,13 +4,12 @@ const PlayerSchema = new mongoose.Schema({
     name:{
         type:String,
         required:[true,"A name is required"],
-        minLength:[2, "*Name must be at least 2 characters in length."]
+        minlength:[2, "*Name must be at least 2 characters in length."]
     },
     position:{
         type:String,
     }
 },{timestamps:true})
-
 
 const Player = mongoose.model('Player',PlayerSchema)
 
